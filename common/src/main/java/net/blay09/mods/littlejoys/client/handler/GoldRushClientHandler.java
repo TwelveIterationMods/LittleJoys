1021a920e14b5aa539a34ab636c210725ea11178
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-04-19 20:14:28 +0200

    fix: Fix all gold rush sounds being stopped even if there are still active gold rushes present

--- common/src/main/java/net/blay09/mods/littlejoys/client/handler/GoldRushClientHandler.java
+++ common/src/main/java/net/blay09/mods/littlejoys/client/handler/GoldRushClientHandler.java
@@ -79,7 +79,9 @@ public class GoldRushClientHandler {
         final var level = minecraft.level;
         if (level != null) {
             activeGoldRushes.remove(level.dimension(), pos);
-            minecraft.getSoundManager().stop(ResourceLocation.fromNamespaceAndPath(LittleJoys.MOD_ID, "gold_rush"), SoundSource.BLOCKS);
+            if (activeGoldRushes.isEmpty()) {
+                minecraft.getSoundManager().stop(ResourceLocation.fromNamespaceAndPath(LittleJoys.MOD_ID, "gold_rush"), SoundSource.BLOCKS);
+            }
         }
     }
 }
